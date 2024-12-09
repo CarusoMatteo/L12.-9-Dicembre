855ebbf51fcce50ea1eea26114ffc7d9b3e55029
Matteo Caruso <matteo.caruso7@studio.unibo.it>
2024-12-09 14:01:01 +0100

    122 - Added Logics interface

---
+++ 122-es2/src/main/java/it/unibo/es2/Logics.java
@@ -0,0 +1,8 @@
+package it.unibo.es2;
+
+public interface Logics {
+
+    String hit(Pair<Integer, Integer> buttonPosition);
+
+    boolean shouldExit();
+}
--- 122-es2/src/main/java/it/unibo/es2/Pair.java
+++ 122-es2/src/main/java/it/unibo/es2/Pair.java
@@ -3,60 +3,59 @@ package it.unibo.es2;
 /*
  * A standard generic Pair<X,Y>, with getters, hashCode, equals, and toString well implemented. 
  */
+public class Pair<X, Y> {
 
-public class Pair<X,Y> {
-	
-	private final X x;
-	private final Y y;
-	
-	public Pair(X x, Y y) {
-		super();
-		this.x = x;
-		this.y = y;
-	}
+    private final X x;
+    private final Y y;
 
-	public X getX() {
-		return x;
-	}
+    public Pair(X x, Y y) {
+        super();
+        this.x = x;
+        this.y = y;
+    }
 
-	public Y getY() {
-		return y;
-	}
+    public X getX() {
+        return x;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((x == null) ? 0 : x.hashCode());
-		result = prime * result + ((y == null) ? 0 : y.hashCode());
-		return result;
-	}
+    public Y getY() {
+        return y;
+    }
 
-	@SuppressWarnings("rawtypes")
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Pair other = (Pair) obj;
-		if (x == null) {
-			if (other.x != null)
-				return false;
-		} else if (!x.equals(other.x))
-			return false;
-		if (y == null) {
-			if (other.y != null)
-				return false;
-		} else if (!y.equals(other.y))
-			return false;
-		return true;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((x == null) ? 0 : x.hashCode());
+        result = prime * result + ((y == null) ? 0 : y.hashCode());
+        return result;
+    }
 
-	@Override
-	public String toString() {
-		return "Pair [x=" + x + ", y=" + y + "]";
-	}
+    @SuppressWarnings("rawtypes")
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Pair other = (Pair) obj;
+        if (x == null) {
+            if (other.x != null)
+                return false;
+        } else if (!x.equals(other.x))
+            return false;
+        if (y == null) {
+            if (other.y != null)
+                return false;
+        } else if (!y.equals(other.y))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Pair [x=" + x + ", y=" + y + "]";
+    }
 }
